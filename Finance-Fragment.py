a2fa69a58043a4ea39268e8e359e89713576ad2b
cleiton <cleitonsantoscorreia06.gmail.com>
2026-09-16 13:00:36 -0300

    feat: versão inicial do cálculo de saldo

---
+++ Finance-Fragment.py
@@ -0,0 +1,19 @@
+print('Finance Fragment')
+print('SEJA BEM VINDO AO SOFTWARE DE ORGANIZAÇÃO FINANCEIRO MAIS SIMPLES E FUNCIONAL.')
+#ENTRADAS
+nome=input('Qual é o seu nome : ')
+receita=float(input('Informe o valor de pelo menos uma receita : '))
+despesa=float(input('informe o valor de uma despesa : '))
+#PROCESSO
+saldo=receita-despesa
+#SAÍDA 
+print('='*30)
+print('Finance Fragment')
+print('='*30)
+print(f'Usuário: {nome}')
+print('-'*30)
+print(f'Receita : R$ {receita:.2f}')
+print(f'Despesa : R$ {despesa:.2f}')
+print('-'*30)
+print(f'Saldo Disponível : R$ {saldo:.2f}')
+print('='*30)
