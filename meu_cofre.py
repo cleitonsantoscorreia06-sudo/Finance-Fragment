55ed3c0cb2fa714d2a0920e99e3443361d1a247a
cleiton <cleitonsantoscorreia06.gmail.com>
2026-09-18 21:00:55 -0300

    feat: adiciona alerta de saldo (positivo, zero, negativo)

--- meu_cofre.py
+++ meu_cofre.py
@@ -16,4 +16,11 @@ print(f'Receita : R$ {receita:.2f}')
 print(f'Despesa : R$ {despesa:.2f}')
 print('-'*30)
 print(f'Saldo Disponível : R$ {saldo:.2f}')
+if saldo >0:
+    print('Saldo positivo : ✅  Parabéns, você está com um bom saldo.')
+elif saldo == 0:
+    print('Saldo zero : Você zerou seu saldo.')
+else:
+    print('Saldo negativo : Atenção você está no vermelho ! ')
+
 print('='*30)
